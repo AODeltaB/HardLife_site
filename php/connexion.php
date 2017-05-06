69a47c2ee7bfabf115a990f98e806dd91ae868b2
Pierre Fortin <aodeltab@gmail.com>
2017-05-06 19:05:59 +0200

    fix

--- php/connexion.php
+++ php/connexion.php
@@ -56,30 +56,6 @@
 		</p>
 	</form>
 
-	<?php
-	// Hachage du mot de passe
-	$pass_hache = sha1($_POST['mdp']);
-
-	// Vérification des identifiants
-	$req = $bdd->prepare('SELECT id FROM membres WHERE pseudo = :pseudo AND mdp = :mdp');
-	$req->execute(array(
-	    'pseudo' => $pseudo,
-	    'mdp' => $pass_hache));
-
-	$resultat = $req->fetch();
-
-	if (!$resultat)
-	{
-	    echo 'Mauvais identifiant ou mot de passe !';
-	}
-	else
-	{
-	    session_start();
-	    $_SESSION['id'] = $resultat['id'];
-	    $_SESSION['pseudo'] = $pseudo;
-	    echo 'Vous êtes connecté !';
-	}
-
   <footer class="page-footer teal">
     <div class="container">
       <div class="row">
--- php/connexion_post.php
+++ php/connexion_post.php
@@ -4,13 +4,20 @@
 	$pass_hache = sha1($_POST['mdp']);
 
 	// Vérification des identifiants
+  $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+  $bdd = new PDO('mysql:host=localhost;dbname=database', 'login', 'mdp',   $pdo_options);
 	$req = $bdd->prepare('SELECT id FROM membres WHERE pseudo = :pseudo AND mdp = :mdp');
 	$req->execute(array(
 	    'pseudo' => $pseudo,
 	    'mdp' => $pass_hache));
 
 	$resultat = $req->fetch();
+  header('Location: connexion.php');
 
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
 	if (!$resultat)
 	{
 	    echo 'Mauvais identifiant ou mot de passe !';
