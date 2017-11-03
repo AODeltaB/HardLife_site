a9445e902ef0746c14455c075080a84cb3e3d752
Pierre <aodeltab@gmail.com>
2017-11-03 12:02:46 +0100

    indent fix

--- php/index.php
+++ php/index.php
@@ -6,10 +6,10 @@ session_start();
 
 <html lang="fr">
 <head>
-<link rel="icon" href="logo.png" />
+  <link rel="icon" href="logo.png" />
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-  <title>HardLife RP</title>
+  <title>Master 2, Droit du Numérique - Caen</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -17,93 +17,93 @@ session_start();
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
- <?php
-if (isset($_SESSION['pseudo'])) {
-?>
-  <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
-      <a id="logo-container" href="index.php" class="brand-logo">HardLife RP</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="index.php">Accueil</a></li>
-		<li><a href="/forum">Forum</a></li>
-        <li><a href="equipe.php">Notre équipe</a></li>
-        <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
-		<li><a href="media.php">Média</a></li>
-	 <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><?php echo htmlentities(trim($_SESSION['pseudo'])); ?><i class="material-icons right">arrow_drop_down</i></a></li>
-	 <li><a class="dropdown-button" href="#!" data-activates="dropdown2">Stats<i class="material-icons right">arrow_drop_down</i></a></li>
-  <!-- Dropdown Structure -->
-  <ul id='dropdown1' class='dropdown-content'>
-    <li><a href="logout.php">Logout</a></li>
-    <li><a href="profile.php">Profil</a></li>
-    <li class="divider"></li>
-  </ul>
-      <ul id='dropdown2' class='dropdown-content'>
-    <li><a href="banlist.php">Banlist</a></li>
-    <li><a href="leaderboard.php">Leaderboard</a></li>
-    <li class="divider"></li>
-  </ul>
-
-</ul>
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="index.php">Acceuil</a></li>
-		<li><a href="/forum">Forum</a></li>
-        <li><a href="equipe.php">Notre équipe</a></li>
-        <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
-		<li><a href="media.php">Média</a></li>
-    <li><a href="logout.php">Logout</a></li>
-    <li><a href="profile.php">Profil</a></li>
-	<li><a href="banlist.php">Banlist</a></li>
-    <li><a href="leaderboard.php">Leaderboard</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-
   <?php
-}
-else {
-?>
-	  <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
-      <a id="logo-container" href="index.php" class="brand-logo">HardLife RP</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="index.php">Accueil</a></li>
-    <li><a href="/forum">Forum</a></li>
-        <li><a href="equipe.php">Notre équipe</a></li>
-        <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
-    <li><a href="media.php">Média</a></li>
+  if (isset($_SESSION['pseudo'])) {
+    ?>
+    <nav class="white" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="index.php" class="brand-logo">Master 2, Droit du Numérique - Caen</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="index.php">Accueil</a></li>
+          <li><a href="/promote.php">Promotions</a></li>
+          <li><a href="equipe.php">Notre équipe</a></li>
+          <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
+          <li><a href="media.php">Média</a></li>
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><?php echo htmlentities(trim($_SESSION['pseudo'])); ?><i class="material-icons right">arrow_drop_down</i></a></li>
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown2">Stats<i class="material-icons right">arrow_drop_down</i></a></li>
+          <!-- Dropdown Structure -->
+          <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="logout.php">Logout</a></li>
+            <li><a href="profile.php">Profil</a></li>
+            <li class="divider"></li>
+          </ul>
+          <ul id='dropdown2' class='dropdown-content'>
+            <li><a href="banlist.php">Banlist</a></li>
+            <li><a href="leaderboard.php">Leaderboard</a></li>
+            <li class="divider"></li>
+          </ul>
 
-   <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Client<i class="material-icons right">arrow_drop_down</i></a></li>
+        </ul>
+        <ul id="nav-mobile" class="side-nav">
+          <li><a href="index.php">Acceuil</a></li>
+          <li><a href="/promote.php">Promotions</a></li>
+          <li><a href="equipe.php">Notre équipe</a></li>
+          <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
+          <li><a href="media.php">Média</a></li>
+          <li><a href="logout.php">Logout</a></li>
+          <li><a href="profile.php">Profil</a></li>
+          <li><a href="banlist.php">Banlist</a></li>
+          <li><a href="leaderboard.php">Leaderboard</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
 
-  <!-- Dropdown Structure -->
-  <ul id='dropdown1' class='dropdown-content'>
-    <li><a href="inscription.php">Inscription</a></li>
-    <li><a href="connexion.php">Connexion</a></li>
-    <li class="divider"></li>
-  </ul>
+    <?php
+  }
+  else {
+    ?>
+    <nav class="white" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="index.php" class="brand-logo">Master 2, Droit du Numérique - Caen</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="index.php">Accueil</a></li>
+          <li><a href="/promote.php">Promotions</a></li>
+          <li><a href="equipe.php">Notre équipe</a></li>
+          <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
+          <li><a href="media.php">Média</a></li>
 
-</ul>
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="index.php">Acceuil</a></li>
-    <li><a href="/forum">Forum</a></li>
-        <li><a href="equipe.php">Notre équipe</a></li>
-        <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
-    <li><a href="media.php">Média</a></li>
-    <li><a href="inscription.php">Inscription</a></li>
-    <li><a href="connexion.php">Connexion</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-  <?php
-}
-?>
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Client<i class="material-icons right">arrow_drop_down</i></a></li>
+
+          <!-- Dropdown Structure -->
+          <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li class="divider"></li>
+          </ul>
+
+        </ul>
+        <ul id="nav-mobile" class="side-nav">
+          <li><a href="index.php">Acceuil</a></li>
+          <li><a href="/promote.php">Promotions</a></li>
+          <li><a href="equipe.php">Notre équipe</a></li>
+          <li><a href="https://discord.gg/h2uhwUW">Discord</a></li>
+          <li><a href="media.php">Média</a></li>
+          <li><a href="inscription.php">Inscription</a></li>
+          <li><a href="connexion.php">Connexion</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+    <?php
+  }
+  ?>
 
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
-        <h1 class="header center teal-text text-lighten-2">HardLife RP</h1>
+        <h1 class="header center teal-text text-lighten-2">Master 2, Droit du Numérique - Caen</h1>
         <div class="row center">
           <h5 class="header col s12 light">Un serveur GTA V RP basé sur le Fun !</h5>
         </div>
@@ -147,7 +147,7 @@ else {
             <h5 class="center">Des Admins à votre écoute !</h5>
 
             <p class="flow-text"> - AO_DeltaB</p>
-			<p class="flow-text"> - DD</p>
+            <p class="flow-text"> - DD</p>
           </div>
         </div>
       </div>
@@ -175,11 +175,11 @@ else {
           <h3><i class="mdi-content-send brown-text"></i></h3>
           <h4>Nous aider ?</h4>
           <p class="left-align light flow-text">Vous voulez nous aider à financer le serveur ?</p>
-		  <p class="left-align light flow-text">Nous disposons d'une adresse PayPal :</p>
-<a href="https://www.paypal.me/hardliferp/">
-<img type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
-<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="2" height="1">
-</a>
+          <p class="left-align light flow-text">Nous disposons d'une adresse PayPal :</p>
+          <a href="https://www.paypal.me/hardliferp/">
+            <img type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+            <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="2" height="1">
+          </a>
         </div>
       </div>
 
@@ -202,7 +202,7 @@ else {
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">HardLife RP</h5>
+          <h5 class="white-text">Master 2, Droit du Numérique - Caen</h5>
           <p class="grey-text text-lighten-4">Nous sommes un serveur à l'écoute de nos joueurs, attentif et particulièrement drôle... ou pas.</p>
 
         </div>
@@ -218,7 +218,7 @@ else {
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+        Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
       </div>
     </div>
   </footer>
@@ -229,5 +229,5 @@ else {
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
 
-  </body>
+</body>
 </html>
